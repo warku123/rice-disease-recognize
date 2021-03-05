b269933c5f4daa3241442575b1dc0003d5638df8
FrankCui <FrankCui713@163.com>
2021-03-05 19:45:53 +0800

    interface fix3.5

--- app/src/main/java/com/example/cogrice/MainActivity.java
+++ app/src/main/java/com/example/cogrice/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.cogrice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //全屏，隐藏手机上方状态栏
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         photo_page_butt = findViewById(R.id.photo_page_butt);
         photo_page_butt.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/cogrice/photopage.java
+++ app/src/main/java/com/example/cogrice/photopage.java
@@ -8,17 +8,35 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.TextPaint;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class photopage extends AppCompatActivity {
-    Button camera_butt;
+    ImageButton camera_butt;
+    TextView tv;
+    TextPaint tp;
+    TextView tv2;
+    TextPaint tp2;
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photopage);
+
+        //全屏，隐藏手机上方状态栏
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //字体加粗
+        tv = (TextView)findViewById(R.id.tip);
+        tp = tv.getPaint();
+        tp.setFakeBoldText(true);
+        tv2 = (TextView)findViewById(R.id.apptitle);
+        tp2 = tv2.getPaint();
+        tp2.setFakeBoldText(true);
 
         camera_butt = findViewById(R.id.take_photo);
         camera_butt.setOnClickListener(new View.OnClickListener() {
