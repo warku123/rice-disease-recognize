f75e99212f1788660424748eb4a25c7f836dcc46
Frank <frank.zhaoxy@gmail.com>
2021-03-16 23:24:54 +0800

    去掉了base64的无用注释

--- app/src/main/java/com/example/cogrice/utils/ImageHelper.java
+++ app/src/main/java/com/example/cogrice/utils/ImageHelper.java
@@ -4,11 +4,19 @@ import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+import java.net.URLEncoder;
 import java.util.Base64;
 
 public class ImageHelper {
-    public static Bitmap base64ToBitmap(String base64ForImage){
-        @SuppressLint("NewApi") byte[] imageByteArray = Base64.getDecoder().decode(base64ForImage);
+
+    @SuppressLint("NewApi")
+    public static String byteArrayToBase64String(byte[] bytes) {// 将图片文件转化为字节数组字符串，并对其进行Base64编码处理
+        return Base64.getEncoder().encodeToString(bytes);
+    }
+
+    @SuppressLint("NewApi")
+    public static Bitmap base64ToBitmap(String base64ForImage) {
+        byte[] imageByteArray = Base64.getDecoder().decode(base64ForImage);
         Bitmap bitmap = BitmapFactory.decodeByteArray(imageByteArray, 0, imageByteArray.length);
         return bitmap;
     }
